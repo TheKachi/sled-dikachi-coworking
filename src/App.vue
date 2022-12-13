6b93013166fdd3bb2b51075b24a136619b7dc76d
Dikachi Nze <nzebenflorence@gmail.com>
2022-12-13 16:06:47 +0100

    feat: barebone done

--- src/App.vue
+++ src/App.vue
@@ -1,71 +1,158 @@
 <template>
 	<div id="app">
-		<div>
-			<div @click="toggleDropdown" class="border w-[250px]">
-				<input
-					class="w-full"
-					type="text"
-					v-model="query"
-					placeholder="Search or select a column"
-				/>
-			</div>
-
-			<ul v-if="isShowing">
-				<li
-					@click.prevent="addColumn(column)"
-					v-for="column in filteredColumns"
-					:key="column"
+		<div class="mx-auto mt-[50px] px-[200px] flex gap-[20px] items-start">
+			<!-- Column Selector Container -->
+			<div class="w-[300px]">
+				<!-- Select Dropdown -->
+				<div
+					@click="isShowing = true"
+					class="text-[14px] px-[16px] py-[10px] flex items-center cursor-pointer border rounded-4 z-0 bg-grey-100 focus:bg-grey-100"
 				>
+					<div class="relative w-full">
+						<!-- Selected columns in order -->
+						<ul
+							class="flex flex-wrap gap-[4px]"
+							v-if="selectedColumns.length"
+						>
+							<li v-for="column in selectedColumns" :key="column">
+								{{ column }}
+								<button @click="removeColumn(column)">X</button>
+							</li>
+						</ul>
+
+						<!-- No column selected  -->
+						<p v-else>Search or select a dropdown</p>
+
+						<!-- Show and Hide icons  -->
+						<div class="absolute z-50 right-0 top-[4px]">
+							<button @click="toggleDropdown" class="">
+								<span v-if="isShowing">
+									<svg
+										width="16"
+										height="16"
+										viewBox="0 0 16 16"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M11.9465 5.45312H7.79317H4.05317C3.41317 5.45312 3.09317 6.22646 3.5465 6.67979L6.99983 10.1331C7.55317 10.6865 8.45317 10.6865 9.0065 10.1331L10.3198 8.81979L12.4598 6.67979C12.9065 6.22646 12.5865 5.45312 11.9465 5.45312Z"
+											fill="#475569"
+										/>
+									</svg>
+								</span>
+
+								<span v-else>
+									<svg
+										width="16"
+										height="16"
+										viewBox="0 0 16 16"
+										fill="none"
+										xmlns="http://www.w3.org/2000/svg"
+									>
+										<path
+											d="M11.9465 5.45312H7.79317H4.05317C3.41317 5.45312 3.09317 6.22646 3.5465 6.67979L6.99983 10.1331C7.55317 10.6865 8.45317 10.6865 9.0065 10.1331L10.3198 8.81979L12.4598 6.67979C12.9065 6.22646 12.5865 5.45312 11.9465 5.45312Z"
+											fill="#475569"
+										/>
+									</svg>
+								</span>
+							</button>
+						</div>
+					</div>
+				</div>
+
+				<!-- Dropdown  -->
+				<ul v-if="isShowing" class="w-full">
 					<input
-						type="checkbox"
-						:value="column"
-						v-model="selectedColumns"
+						class="w-full focus:border-transparent focus:ring-0"
+						type="text"
+						v-model="query"
+						placeholder="Search"
 					/>
-					{{ column }}
-				</li>
-			</ul>
-			<div v-if="selectedColumns.length > 0">
-				<ul v-for="column in selectedColumns" :key="column">
-					<li>
-						<button @click="removeColumn(column)">X</button>
+					<!-- @click="$emit('add', column)" -->
+					<li
+						@click="addColumn(column)"
+						v-for="column in filteredColumns"
+						:key="column"
+					>
 						{{ column }}
 					</li>
 				</ul>
-			</div>
-		</div>
-		<!-- <ColumnSelector
+
+				<!-- Commented out Column selector component  -->
+				<!-- <ColumnSelector
 			:columns="columns"
 			:selected-columns="selectedColumns"
 			@search="query = $event"
 			@add="addColumn"
 			@remove="removeColumn"
 		/> -->
+			</div>
+
+			<button @click="toggleDropdown" class="border p-1">
+				<span v-if="isShowing">Hide</span>
+				<span v-else>Show</span>
+			</button>
+
+			<!-- Show Dialog button  -->
+			<button
+				id="show-modal"
+				@click="showDialog = true"
+				class="border px-[24px] py-[8px]"
+			>
+				Show Modal
+			</button>
+		</div>
+
+		<!-- Dialog  -->
+		<Dialog v-if="showDialog" @close="showDialog = false"> </Dialog>
 	</div>
 </template>
 
 <script>
 // import ColumnSelector from "./components/ColumnSelector.vue";
+import Dialog from "./components/Dialog.vue";
 
 export default {
 	name: "App",
 	data() {
 		return {
-			columns: ["ID", "Total", "Links", "Number", "Status"],
+			columns: [
+				"ID",
+				"Total",
+				"Links",
+				"Number",
+				"Status",
+				"Events",
+				"Onboarding",
+				"Maintenace",
+				"Adverisinf",
+			],
 			selectedColumns: [],
 			query: "",
 			isShowing: false,
+			showDialog: false,
 		};
 	},
 	methods: {
+		// showDropdown() {
+		// 	this.isShowing = true;
+		// },
+
+		// closeDropdown() {
+		// 	toggleDropdown();
+		// 	alert(this.isShowing);
+		// },
+
 		toggleDropdown() {
-			this.isShowing = true;
+			this.isShowing = !this.isShowing;
 		},
+
 		// add column to selectedColumns array
 		addColumn(column) {
 			if (this.selectedColumns.includes(column)) return;
-			this.selectedColumns.push(column);
-			// this.isShowing = false;
+			this.selectedColumns.unshift(column);
 		},
+
 		// remove column from selectedColumns array
 		removeColumn(column) {
 			this.selectedColumns = this.selectedColumns.filter(
@@ -80,11 +167,11 @@ export default {
 				column.toLowerCase().includes(this.query.toLowerCase())
 			);
 		},
-		// orderedSelectedColumns() {
-		// 	return this.selectedColumns.sort();
-		// },
 	},
-	// components: { ColumnSelector },
+	components: {
+		// ColumnSelector,
+		Dialog,
+	},
 };
 </script>
 
---
+++ src/components/Dialog.vue
@@ -0,0 +1,79 @@
+<template>
+	<div
+		class="fixed top-60 z-[100] inset-0 overflow-y-auto"
+		role="dialog"
+		aria-modal="true"
+	>
+		<div>
+			<div
+				class="fixed inset-0 bg-black/60"
+				aria-hidden="true"
+				@click="$emit('close')"
+			></div>
+			<!-- <span
+				class="hidden sm:inline-block sm:align-middle sm:h-screen"
+				aria-hidden="true"
+				>&#8203;</span
+			> -->
+			<div
+				class="mx-auto bg-white text-left rounded-lg overflow-hidden shadow-[0_4px_50px_rgba(0,0,0,0.1)] transform transition-all"
+				:class="[width, height]"
+			>
+				<div
+					class="px-[24px] py-[12px] min-w-full min-h-full flex flex-col"
+					:class="[width, height]"
+				>
+					<!-- Header  -->
+					<div class="flex justify-between items-center">
+						<slot name="header"> default header </slot>
+						<button
+							@click="$emit('close')"
+							class="text-[32px] float-right"
+						>
+							x
+						</button>
+					</div>
+
+					<!-- Body -->
+					<div class="">
+						<slot name="body"> default body </slot>
+					</div>
+
+					<!-- Footer  -->
+					<div class="modal-footer border mt-auto">
+						<slot name="footer">
+							<button
+								class="modal-default-button"
+								@click="$emit('close')"
+							>
+								OK
+							</button>
+						</slot>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+export default {
+	name: "Dialog",
+	props: {
+		classNames: {
+			type: String,
+			default: "",
+		},
+		width: {
+			type: String,
+			default: "w-[500px]",
+		},
+		height: {
+			type: String,
+			default: "h-[500px]",
+		},
+	},
+};
+</script>
+
+<style></style>
